64b5c81f221673e8eec071d8676588ec82752ae5
hmm <hmmChase@users.noreply.github.com>
2019-02-23 10:21:41 -0800

    Update

--- frontend/components/Nav/Nav.js
+++ frontend/components/Nav/Nav.js
@@ -3,7 +3,10 @@ import Link from 'next/link';
 const Nav = () => (
   <nav>
     <Link href="/">
-      <a>Home</a>
+      <a>Home </a>
+    </Link>
+    <Link href="examples">
+      <a>Examples</a>
     </Link>
   </nav>
 );
