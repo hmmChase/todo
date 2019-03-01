cce3ad464fc1f76e9674f7df0636bd89ac13f7cb
hmm <hmmChase@users.noreply.github.com>
2019-03-01 13:58:53 -0800

    Update

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -1,8 +1,8 @@
 const { ApolloServer } = require('apollo-server');
 const { importSchema } = require('graphql-import');
 const path = require('path');
-const { prisma } = require('../prisma/generated/prisma-client');
 const typeDefs = importSchema(path.resolve('src/schema.graphql'));
+const { prisma } = require('../prisma/generated/prisma-client');
 const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
 
@@ -13,6 +13,10 @@ function createServer() {
       Query,
       Mutation
     },
+    context: req => ({
+      ...req,
+      prisma
+    }),
     introspection: true,
     playground: true
   });
