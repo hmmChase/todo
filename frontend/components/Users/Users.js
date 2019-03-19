611d9179e0113aaf92cc10f7b3dd831d6671af73
hmm <hmmChase@users.noreply.github.com>
2019-03-19 14:50:19 -0700

    Update

--- frontend/components/Users/Users.js
+++ frontend/components/Users/Users.js
@@ -1,26 +1,17 @@
-import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
+import * as userQL from '../../graphql/Queries/user';
 
-export const USERS_QUERY = gql`
-  query USERS_QUERY {
-    users {
-      id
-      name
-    }
-  }
-`;
-
-class QueryExample extends React.PureComponent {
+class Users extends React.PureComponent {
   render() {
     return (
-      <Query query={USERS_QUERY}>
-        {({ data, error, loading }) => {
+      <Query query={userQL.USERS_QUERY}>
+        {({ data, loading, error }) => {
           if (loading) return <p>Loading...</p>;
           if (error) return <p>Error: {error.message}</p>;
           return (
             <ul>
               {data.users.map(user => (
-                <li key={user.id}>{user.name}</li>
+                <li key={user.id}>{user.email}</li>
               ))}
             </ul>
           );
@@ -30,4 +21,4 @@ class QueryExample extends React.PureComponent {
   }
 }
 
-export default QueryExample;
+export default Users;
---
+++ frontend/pages/users.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Users from '../components/Users/Users';
+
+const UsersPage = React.memo(() => (
+  <>
+    <Head>
+      <title>next-graphql-starter | Users</title>
+    </Head>
+    <Users />
+  </>
+));
+
+export default UsersPage;
