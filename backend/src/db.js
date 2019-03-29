858b9eb3e3b3ac52a6771f45b1709260095c6466
hmm <hmmChase@users.noreply.github.com>
2019-03-29 14:12:00 -0700

    Update

--- backend/src/db.js
+++ backend/src/db.js
@@ -3,9 +3,9 @@ const { Prisma } = require('prisma-binding');
 
 const db = new Prisma({
   typeDefs: __dirname + '/schema_prep.graphql',
-  endpoint: process.env.PRISMA_ENDPOINT,
-  secret: process.env.PRISMA_SECRET,
-  debug: false
+  endpoint: process.env.PRISMA_ENDPOINT
+  // secret: process.env.PRISMA_SECRET,
+  // debug: false
 });
 
 module.exports = db;
