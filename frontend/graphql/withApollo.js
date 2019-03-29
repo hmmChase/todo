85e331dbb58152a727aa198e99ca9a555a0ed01b
hmm <hmmChase@users.noreply.github.com>
2019-03-29 13:09:52 -0700

    Update

--- frontend/graphql/withApollo.js
+++ frontend/graphql/withApollo.js
@@ -10,39 +10,39 @@ import { devGraphQLEndpoint, prodGraphQLEndpoint } from '../config';
 
 export default withApollo(
   ({ ctx, headers, initialState }) => {
-    const errorLink = onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors)
-        graphQLErrors.map(({ message, locations, path }) =>
-          console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-          )
-        );
-      if (networkError) {
-        console.log(`[Network error]: ${networkError}`);
-      }
-    });
+    // const errorLink = onError(({ graphQLErrors, networkError }) => {
+    //   if (graphQLErrors)
+    //     graphQLErrors.map(({ message, locations, path }) =>
+    //       console.log(
+    //         `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+    //       )
+    //     );
+    //   if (networkError) {
+    //     console.log(`[Network error]: ${networkError}`);
+    //   }
+    // });
 
-    const authLink = new ApolloLink((operation, forward) => {
-      operation.setContext({
-        headers
-      });
-      return forward(operation);
-    });
+    // const authLink = new ApolloLink((operation, forward) => {
+    //   operation.setContext({
+    //     headers
+    //   });
+    //   return forward(operation);
+    // });
 
     const httpLink = createHttpLink({
       uri:
-        process.env.NODE_ENV === 'development'
-          ? devGraphQLEndpoint
-          : prodGraphQLEndpoint
+        process.env.NODE_ENV === 'production'
+          ? prodGraphQLEndpoint
+          : devGraphQLEndpoint
       // credentials: 'same-origin',
       // credentials: 'include',
     });
 
-    console.log('process.env.NODE_ENV: ', process.env.NODE_ENV)
-
+    console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
 
     return new ApolloClient({
-      link: ApolloLink.from([errorLink, authLink, httpLink]),
+      link: httpLink,
+      // link: ApolloLink.from([errorLink, authLink, httpLink]),
       cache: new InMemoryCache().restore(initialState || {}),
       ssrMode: !process.browser,
       ssrForceFetchDelay: 100,
