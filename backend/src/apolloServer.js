50046a98aab6fb45c51bc16e41b6b62d746a2eed
hmm <hmmChase@users.noreply.github.com>
2019-04-01 08:17:07 -0700

    Update

---
+++ backend/src/apolloServer.js
@@ -0,0 +1,32 @@
+import { ApolloServer } from 'apollo-server-express';
+import { importSchema } from 'graphql-import';
+import jwt from 'jsonwebtoken';
+// import { prisma } from '../prisma/generated/prisma-client';
+const prisma = require('./prismaClient');
+import resolvers from './resolvers';
+import * as auth from '../utils/auth';
+// const typeDefs = importSchema('src/schema/schema.graphql');
+const typeDefs = importSchema(__dirname + '/schema/schema.graphql');
+
+const dev = process.env.NODE_ENV === 'development';
+
+export default () =>
+  new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: async ({ req, res }) => {
+      // const me = await auth.getMe(req.cookies);
+
+      return { req, res, prisma };
+    },
+    tracing: dev,
+    introspection: dev,
+    playground: dev
+      ? {
+          settings: {
+            'editor.theme': 'light'
+            // 'request.credentials': 'include'
+          }
+        }
+      : false
+  });
---
+++ backend/utils/auth.js
@@ -0,0 +1,13 @@
+import { AuthenticationError } from 'apollo-server-express';
+
+export const getMe = async cookies => {
+  const { token } = cookies;
+
+  if (token) {
+    try {
+      return await jwt.verify(token, process.env.JWT_SECRET);
+    } catch (e) {
+      throw new AuthenticationError('Your session expired. Sign in again.');
+    }
+  }
+};
