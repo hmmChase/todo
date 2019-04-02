8a8a8294fda552e8175abf318e4b832839db7029
hmm <hmmChase@users.noreply.github.com>
2019-04-02 08:41:12 -0700

    Update

--- frontend/components/Users/Users.js
+++ frontend/components/Users/Users.js
@@ -1,10 +1,11 @@
 import { Query } from 'react-apollo';
-import * as userQL from '../../graphql/queries/user';
+// import * as userQL from '../../graphql/queries/user';
+import * as query from './Users.query';
 
 class Users extends React.PureComponent {
   render() {
     return (
-      <Query query={userQL.USERS_QUERY}>
+      <Query query={query.USERS_QUERY}>
         {({ data, loading, error }) => {
           if (loading) return <p>Loading...</p>;
           if (error) return <p>Error: {error.message}</p>;
