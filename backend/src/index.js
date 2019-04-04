159d8fae809d073276237447210242755295aa1e
hmm <hmmChase@users.noreply.github.com>
2019-04-04 08:13:53 -0700

    Update

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,9 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-// import helmet from 'helmet';
+import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import apolloServer from './apolloServer';
-// import jwt from 'jsonwebtoken';
 
 const app = express();
 const server = apolloServer();
@@ -29,32 +28,9 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-// app.use(helmet());
+app.use(helmet());
 app.use(cookieParser());
 
-// // Decode the JWT so we can get the user Id on each request
-// app.use((req, res, next) => {
-//   const { token } = req.cookies;
-
-//   if (token) {
-//     const { userId } = jwt.verify(token, process.env.JWT_SECRET);
-//     // Put the userId onto the req for future requests to access
-//     req.userId = userId;
-//   }
-
-//   next();
-// });
-
-// 2. Create a middleware that populates the user on each request
-// app.use(async (req, res, next) => {
-//   // if they aren't logged in, skip this
-//   if (!req.userId) return next();
-//   const user = await prisma.user({ where: { id: req.userId } });
-//   req.user = user;
-//   next();
-// });
-
-// cors: corsOptions }
 server.applyMiddleware({ app, path: '/graphql', cors: corsOptions });
 
 app.listen({ port: process.env.PORT || 4000 }, err => {
--- backend/src/utils/auth.js
+++ backend/src/utils/auth.js
@@ -11,9 +11,9 @@ export const signJWT = async payload =>
 export const verifyJWT = async token => {
   try {
     return await jwt.verify(token, process.env.JWT_SECRET);
-  } catch (e) {
+  } catch (err) {
     throw new AuthenticationError(
-      `Your session expired. Sign in again. (error: ${e})`
+      `Your session expired. Sign in again. (${err})`
     );
   }
 };
