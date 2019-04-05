420b37b6e99653177082dbb9690835333f9ecc06
hmm <hmmChase@users.noreply.github.com>
2019-04-05 16:30:26 -0700

    Update

--- frontend/graphql/withApollo.js
+++ frontend/graphql/withApollo.js
@@ -3,9 +3,10 @@
 import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
+import { BatchHttpLink } from 'apollo-link-batch-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { onError } from 'apollo-link-error';
-import { ApolloLink } from 'apollo-link';
+import { ApolloLink, split } from 'apollo-link';
 import { devGraphQLEndpoint, prodGraphQLEndpoint } from '../config';
 
 export default withApollo(
@@ -16,7 +17,10 @@ export default withApollo(
       if (graphQLErrors)
         graphQLErrors.map(({ message, locations, path }) =>
           console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+            `[GraphQL error]:
+              Message: ${message},
+              Location: ${locations},
+              Path: ${path}`
           )
         );
       if (networkError) {
@@ -31,17 +35,41 @@ export default withApollo(
       return forward(operation);
     });
 
+    const uri =
+      process.env.NODE_ENV === 'production'
+        ? prodGraphQLEndpoint
+        : devGraphQLEndpoint;
+
+    // link to use if batching (default)
+    const batchHttpLink = new BatchHttpLink({
+      uri,
+      credentials: 'include',
+      headers: { batch: 'true ' }
+    });
+
+    // link to use if not batching
     const httpLink = createHttpLink({
-      uri:
-        process.env.NODE_ENV === 'production'
-          ? prodGraphQLEndpoint
-          : devGraphQLEndpoint,
+      uri,
       credentials: 'include'
     });
 
+    const link = ApolloLink.from([
+      errorLink,
+      authLink,
+      // Using the ability to split links, you can send data to each link
+      // depending on what kind of operation is being sent
+      split(
+        operation => operation.getContext().important === true,
+        httpLink, // if the test is true -- debatch
+        batchHttpLink // otherwise, batching is fine
+      )
+    ]);
+
+    const cache = new InMemoryCache().restore(initialState || {});
+
     return new ApolloClient({
-      link: ApolloLink.from([errorLink, authLink, httpLink]),
-      cache: new InMemoryCache().restore(initialState || {}),
+      link,
+      cache,
       ssrMode: !process.browser,
       ssrForceFetchDelay: 100,
       connectToDevTools: process.browser
