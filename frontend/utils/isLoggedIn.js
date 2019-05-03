bc8835eafcbfb4b286b7ee6b700d88f73b295c69
hmm <hmmChase@users.noreply.github.com>
2019-05-03 10:15:42 -0700

    Update

--- frontend/utils/isLoggedIn.js
+++ frontend/utils/isLoggedIn.js
@@ -1,14 +1,13 @@
-/* eslint-disable no-console */
 import * as query from '../components/wrappers/WithUser/WithUser.query';
 
 export const isLoggedIn = async client => {
   try {
-    const res = await client.query({
-      query: query.ME_QUERY
-    });
+    const {
+      data: { me }
+    } = await client.query({ query: query.ME_QUERY });
 
-    return res.data.me;
-  } catch (err) {
+    return me;
+  } catch {
     return null;
   }
 };
