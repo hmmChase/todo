05195c59d2fee4bf14356580ef181d6ae7b0a200
Chase <hmmChase@users.noreply.github.com>
2019-06-02 16:39:46 -0700

    Update

--- backend/src/prisma/prismaClient.js
+++ backend/src/prisma/prismaClient.js
@@ -1,10 +1,10 @@
 import { Prisma } from 'prisma-binding';
 
 const prisma = new Prisma({
-  // typeDefs: './src/schema/schema_prep.graphql',
+  typeDefs: 'src/schema/schema_prep.graphql',
   // typeDefs: 'src/schema/generated/prisma.graphql',
   // typeDefs: __dirname + '/schema/generated/prisma.graphql',
-  typeDefs: __dirname + '/schema/schema_prep.graphql',
+  // typeDefs: __dirname + '/schema/schema_prep.graphql',
   endpoint: process.env.PRISMA_ENDPOINT,
   secret: process.env.PRISMA_SECRET
 });
