89a1b882afee70dd38787e999f8fc7a25a8bae6d
Chase <hmmChase@users.noreply.github.com>
2019-06-03 15:51:03 -0700

    Update

--- backend/src/schema/index.js
+++ backend/src/schema/index.js
@@ -3,7 +3,7 @@ import { importSchema } from 'graphql-import';
 import resolvers from '../resolvers';
 
 // const typeDefs = importSchema(__dirname + '/schema.graphql');
-const typeDefs = importSchema('./src/schema/schema.graphql');
+const typeDefs = importSchema('src/schema/schema.graphql');
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
