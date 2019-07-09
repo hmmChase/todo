9429b988a14cb8a495f3008ac8a24cbcd479440c
Chase <hmmChase@users.noreply.github.com>
2019-07-09 16:52:04 -0700

    Update

---
+++ frontend/components/LoggedOutIdeaWidget/LoggedOutIdeaWidget.js
@@ -0,0 +1,9 @@
+import * as sc from './LoggedOutIdeaWidget.style';
+
+const NoIdeas = React.memo(() => (
+  <sc.NoIdeas>
+    <p>Sign in to start adding ideas</p>
+  </sc.NoIdeas>
+));
+
+export default NoIdeas;
---
+++ frontend/components/LoggedOutIdeaWidget/LoggedOutIdeaWidget.style.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const NoIdeas = styled.div`
+  background-color: black;
+`;
