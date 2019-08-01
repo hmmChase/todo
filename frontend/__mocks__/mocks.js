1963df1e3a7e3725c56132577a1b23ce26f66446
Chase <hmmChase@users.noreply.github.com>
2019-08-01 09:25:39 -0700

    Update

--- frontend/__mocks__/mocks.js
+++ frontend/__mocks__/mocks.js
@@ -1,17 +1,8 @@
-export const user = {
-  id: '1',
-  email: 'a@a.com'
-};
+export const user = { id: '1', email: 'a@a.com' };
 
-export const idea = {
-  id: '1',
-  content: 'mock content'
-};
+export const idea = { id: '1', content: 'mock content' };
 
-export const newIdea = {
-  id: '1',
-  content: 'new mock content'
-};
+export const newIdea = { id: '1', content: 'new mock content' };
 
 export const users = [user];
 export const ideas = [user];
