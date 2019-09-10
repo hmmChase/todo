d7c4447a9831aa6dc06edd028821fa31b287fd76
Chase <hmmChase@users.noreply.github.com>
2019-09-09 18:18:42 -0700

    Update

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,47 +2,26 @@
 /* eslint-disable no-param-reassign */
 // https://nextjs.org/docs#customizing-webpack-config
 
+const fs = require('fs');
 const path = require('path');
 const withPlugins = require('next-compose-plugins');
-const withCSS = require('@zeit/next-css');
 const DotenvWebpackPlugin = require('dotenv-webpack');
 const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+const withCSS = require('@zeit/next-css');
+const withLess = require('@zeit/next-less');
+const lessToJS = require('less-vars-to-js');
+
+// Where your antd-custom.less file lives
+const themeVariables = lessToJS(
+  fs.readFileSync(path.resolve(__dirname, './styles/antd-custom.less'), 'utf8')
+);
 
 const nextConfig = {
   target: 'serverless',
 
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.plugins = config.plugins || [];
-
-    // https://github.com/zeit/next.js/tree/canary/examples/with-dotenv
-    config.plugins.push(
-      new DotenvWebpackPlugin({
-        path: path.join(__dirname, '.env'),
-        systemvars: true
-      })
-    );
-
-    // https://github.com/zeit/next.js/tree/canary/examples/with-ant-design
-    if (isServer) {
-      const antStyles = /antd\/.*?\/style\/css.*?/;
-      const origExternals = [...config.externals];
-
-      config.externals = [
-        (context, request, callback) => {
-          if (request.match(antStyles)) return callback();
-          if (typeof origExternals[0] === 'function') {
-            origExternals[0](context, request, callback);
-          } else {
-            callback();
-          }
-        },
-        ...(typeof origExternals[0] === 'function' ? [] : origExternals)
-      ];
-
-      config.module.rules.unshift({ test: antStyles, use: 'null-loader' });
-    }
-
-    return config;
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+    modifyVars: themeVariables // make your antd custom effective
   },
 
   // github.com/zeit/next-plugins/tree/master/packages/next-bundle-analyzer
@@ -57,7 +36,46 @@ const nextConfig = {
       analyzerMode: 'static',
       reportFilename: '../bundles/client.html'
     }
+  },
+
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.plugins = config.plugins || [];
+
+    // https://github.com/zeit/next.js/tree/canary/examples/with-dotenv
+    config.plugins.push(
+      new DotenvWebpackPlugin({
+        path: path.join(__dirname, '.env'),
+        systemvars: true
+      })
+    );
+
+    // https://github.com/zeit/next.js/blob/canary/examples/with-ant-design-less/next.config.js
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style.*?/;
+      const origExternals = [...config.externals];
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback();
+          if (typeof origExternals[0] === 'function') {
+            origExternals[0](context, request, callback);
+          } else {
+            callback();
+          }
+        },
+        ...(typeof origExternals[0] === 'function' ? [] : origExternals)
+      ];
+
+      config.module.rules.unshift({
+        test: antStyles,
+        use: 'null-loader'
+      });
+    }
+
+    return config;
   }
 };
 
-module.exports = withPlugins([withCSS, withBundleAnalyzer], nextConfig);
+module.exports = withPlugins(
+  [withCSS, withLess, withBundleAnalyzer],
+  nextConfig
+);
