e97d013145b05825e2e5feaa96c5aba0566807fd
Chase <hmmChase@users.noreply.github.com>
2019-11-22 11:55:46 -0800

    Update

--- frontend/styles/theme.style.js
+++ frontend/styles/theme.style.js
@@ -1,11 +1,9 @@
 export default {
   color: {
     text: '#393939',
-    black: '#393939',
-    blue: '#ACE',
+    black: 'black',
+    lightBlue: '#C8DCF0',
     green: 'limegreen',
-    orange: 'tomato',
-    purple: 'blueviolet',
-    yellow: 'yellow'
+    darkRed: '#8A0000'
   }
 };
