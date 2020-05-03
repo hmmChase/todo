d25d33f233216089eb16ee400de94e56ae545761
Chase <hmmChase@users.noreply.github.com>
2020-05-03 15:38:50 -0700

    Update

--- frontend/components/DetailBtn.js
+++ frontend/components/DetailBtn.js
@@ -1,31 +1,14 @@
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-// import { useRouter } from 'next/router';
 
-const DetailIcon = (props) => {
-  // const router = useRouter();
+const DetailBtn = (props) => (
+  <Link href='/idea/[id]' as={`/idea/${props.id}`}>
+    <button aria-label='detail button'>></button>
+  </Link>
+);
 
-  // <Link href={{ pathname: '/idea', query: { id: props.id } }}>
-  // as={`/idea/${props.id}`}
-
-  return (
-    // ex: http://localhost:8008/idea?id=ck5h7pmp1v1300b009uskbj2k
-    <Link href={`/idea/${props.id}`}>
-      <a aria-label='detail button'>
-        <button
-        // onClick={() =>
-        //   router.push('/idea', `idea?id=${props.id}`, { id: props.id })
-        // }
-        >
-          >
-        </button>
-      </a>
-    </Link>
-  );
-};
-
-DetailIcon.propTypes = {
+DetailBtn.propTypes = {
   id: PropTypes.string.isRequired,
 };
 
-export default React.memo(DetailIcon);
+export default React.memo(DetailBtn);
