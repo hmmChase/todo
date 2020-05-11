3e4cc3e7c010d3fe210e11f7e610b0981e6b1106
Chase <hmmChase@users.noreply.github.com>
2020-05-11 10:14:25 -0700

    Update

--- frontend/components/ResetPassword.js
+++ frontend/components/ResetPassword.js
@@ -1,20 +1,12 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-import Link from 'next/link';
-// import Router from 'next/router';
 import { useMutation } from '@apollo/react-hooks';
 // import { useMutation } from '@apollo/client';
-import {
-  passwordRequirements,
-  passResetTokenMissingError,
-  passResetTokenExpiredError,
-  passResetSuccessful,
-} from '../config';
+import { passwordRequirements } from '../config';
 import { RESET_PASSWORD } from '../graphql/queries';
+import ResetPassSuccess from './ResetPassSuccess';
 
 const ResetPassword = (props) => {
-  const { resetToken, resetTokenExpiry } = props;
-
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [isSubmitDisabled, setIsSubmitDisabled] = useState(
@@ -44,111 +36,71 @@ const ResetPassword = (props) => {
   const onSubmit = (e) => {
     e.preventDefault();
 
-    resetPassword({ variables: { resetToken, password, confirmPassword } });
+    resetPassword({
+      variables: { resetToken: props.resetToken, password, confirmPassword },
+    });
   };
 
-  const isTokenPresent = !!(resetToken && resetTokenExpiry);
-  const isTokenExpired = Date.now() > resetTokenExpiry;
-  // const isTokenValid = isTokenPresent && !isTokenExpired;
+  if (!error && data && data.resetPassword) return <ResetPassSuccess />;
 
-  if (!isTokenPresent)
-    return (
+  return (
+    <form onSubmit={onSubmit}>
       <fieldset>
         <h2>Reset Password</h2>
 
-        <p>{passResetTokenMissingError}</p>
+        <div>
+          <label htmlFor='password'>
+            New Password
+            <input
+              name='password'
+              placeholder='password'
+              type='password'
+              value={password}
+              onChange={onChange}
+            />
+          </label>
+        </div>
 
-        <Link href={{ pathname: '/welcome' }}>
-          <button aria-label='back button'>Back</button>
-        </Link>
+        <div>
+          <label htmlFor='confirmPassword'>
+            Confirm New Password
+            <input
+              name='confirmPassword'
+              placeholder='password'
+              type='password'
+              value={confirmPassword}
+              onChange={onChange}
+            />
+          </label>
+        </div>
+
+        <h3>{passwordRequirements.title}</h3>
+
+        <ul>
+          {passwordRequirements.reqs.map((req, i) => (
+            <li key={i}>{req}</li>
+          ))}
+        </ul>
+
+        <button
+          aria-label='reset password'
+          type='submit'
+          disabled={loading || isSubmitDisabled}
+        >
+          Reset
+        </button>
+
+        {error &&
+          error.graphQLErrors.map((graphQLError, i) => (
+            <p key={i}>{graphQLError.message}</p>
+          ))}
       </fieldset>
-    );
-  else if (isTokenPresent && isTokenExpired)
-    return (
-      <fieldset>
-        <h2>Reset Password</h2>
-
-        <p>{passResetTokenExpiredError}</p>
-
-        <Link href={{ pathname: '/welcome' }}>
-          <button aria-label='back button'>Back</button>
-        </Link>
-      </fieldset>
-    );
-  else if (!error && data && data.resetPassword)
-    return (
-      <fieldset>
-        <h2>Reset Password</h2>
-
-        <p>{passResetSuccessful}</p>
-
-        <Link href={{ pathname: '/' }}>
-          <button aria-label='home button'>Home</button>
-        </Link>
-      </fieldset>
-    );
-  else
-    return (
-      <form onSubmit={onSubmit}>
-        <fieldset>
-          <h2>Reset Password</h2>
-
-          <div>
-            <label htmlFor='password'>
-              New Password
-              <input
-                name='password'
-                placeholder='password'
-                type='password'
-                value={password}
-                onChange={onChange}
-              />
-            </label>
-          </div>
-
-          <div>
-            <label htmlFor='confirmPassword'>
-              Confirm New Password
-              <input
-                name='confirmPassword'
-                placeholder='password'
-                type='password'
-                value={confirmPassword}
-                onChange={onChange}
-              />
-            </label>
-          </div>
-
-          <h3>{passwordRequirements.title}</h3>
-
-          <ul>
-            {passwordRequirements.reqs.map((req, i) => (
-              <li key={i}>{req}</li>
-            ))}
-          </ul>
-
-          <button
-            aria-label='reset password'
-            type='submit'
-            disabled={loading || isSubmitDisabled}
-          >
-            Reset
-          </button>
-
-          {error &&
-            error.graphQLErrors.map((graphQLError, i) => (
-              <p key={i}>{graphQLError.message}</p>
-            ))}
-        </fieldset>
-      </form>
-    );
+    </form>
+  );
 };
-
-ResetPassword.defaultProps = { resetToken: '', resetTokenExpiry: '' };
 
 ResetPassword.propTypes = {
   resetToken: PropTypes.string.isRequired,
-  resetTokenExpiry: PropTypes.string.isRequired,
 };
 
 export default React.memo(ResetPassword);
--- frontend/pages/resetpassword.js
+++ frontend/pages/resetpassword.js
@@ -2,27 +2,37 @@ import PropTypes from 'prop-types';
 import withApollo from '../graphql/withApollo';
 import signedIn from '../utils/signedIn';
 import redirect from '../utils/redirect';
+import ResetPassError from '../components/ResetPassError';
 import ResetPassword from '../components/ResetPassword';
 
-const ResetPasswordPage = (props) => (
-  <ResetPassword
-    resetToken={props.resetToken}
-    resetTokenExpiry={props.resetTokenExpiry}
-  />
-);
+const ResetPasswordPage = (props) => {
+  const isTokenPresent = !!(props.resetToken && props.resetTokenExpiry);
+  const isTokenExpired = Date.now() > props.resetTokenExpiry;
+
+  if (!isTokenPresent || isTokenExpired)
+    return (
+      <ResetPassError
+        isTokenPresent={isTokenPresent}
+        isTokenExpired={isTokenExpired}
+      />
+    );
+
+  return <ResetPassword resetToken={props.resetToken} />;
+};
 
 ResetPasswordPage.getInitialProps = async (ctx) => {
   const { req, res, query } = ctx;
   const { resetToken, resetTokenExpiry } = query;
 
   /* must not be signed in */
-  if (signedIn(req)) redirect(res, '/');
-  else return { resetToken, resetTokenExpiry };
+  if (req && signedIn(req)) redirect(res, '/');
+
+  return { resetToken, resetTokenExpiry };
 };
 
 ResetPasswordPage.propTypes = {
-  resetToken: PropTypes.string.isRequired,
-  resetTokenExpiry: PropTypes.string.isRequired,
+  resetToken: PropTypes.string,
+  resetTokenExpiry: PropTypes.string,
 };
 
 export default withApollo({ ssr: true })(ResetPasswordPage);
