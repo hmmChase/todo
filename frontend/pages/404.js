21d056d5bd8744e1137db6c77e2714feb0016878
Chase <hmmChase@users.noreply.github.com>
2020-06-08 09:14:42 -0700

    Update

--- frontend/pages/404.js
+++ frontend/pages/404.js
@@ -3,13 +3,14 @@
 import withApollo from '../graphql/withApollo';
 import Layout from '../components/Layout';
 import Header from '../components/Header';
+import BackBtn from '../components/BackBtn';
 import Footer from '../components/Footer';
 
 const FourOhFourPage = () => (
   <Layout
     title='404'
     header={<Header title='404' />}
-    content={<p>404</p>}
+    content={<BackBtn path='/' />}
     footer={<Footer />}
   />
 );
