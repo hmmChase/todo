ec278418898a0d9cc4580440e4c661e81bf6d3b1
Chase <hmmChase@users.noreply.github.com>
2020-06-28 09:23:14 -0700

    Update

--- frontend/config.js
+++ frontend/config.js
@@ -1,12 +1,12 @@
 export const title = 'hmmStart';
 
-export const frontendURLProdCurrent = process.env.VERCEL_URL;
+export const deployedUrl = process.env.VERCEL_URL;
 
 export const graphqlUrlDev = 'http://localhost:6969/api/graphql';
-export const graphqlUrlProd = `https://${frontendURLProdCurrent}/api/graphql`;
+export const graphqlUrlProd = `https://${deployedUrl}/api/graphql`;
 
 export const refreshUrlDev = 'http://localhost:6969/api/refresh';
-export const refreshUrlProd = `https://${frontendURLProdCurrent}/api/refresh`;
+export const refreshUrlProd = `https://${deployedUrl}/api/refresh`;
 
 export const ideasPerPage = 5;
 
