708b0f08f98b2cabc911358920c41be37bd3997a
hmmChase <hmmChase@users.noreply.github.com>
2021-07-14 19:27:48 -0700

    Update

--- backend/src/utils/accessToken.js
+++ backend/src/utils/accessToken.js
@@ -15,14 +15,13 @@ export const verifyAccessToken = accessToken => {
   const secret = Buffer.from(process.env.ACCESS_TOKEN_SECRET, 'base64');
 
   try {
-    // Return the decoded payload if the signature is valid and JWT not expired
+    // Decode payload if signature is valid and JWT not expired
     const payload = jwt.verify(accessToken, secret);
 
-    console.log('payload:', payload);
-
+    // Return payload
     return payload;
   } catch (error) {
-    console.log('error: ', error);
+    console.log('verifyAccessToken error : ', error);
 
     // If not, throw error
     throw new AuthenticationError('user.invalidCredentials');
