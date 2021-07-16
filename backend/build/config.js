387bdf3e7d2ee0995ddebbddf33b353da99e971b
hmmChase <hmmChase@users.noreply.github.com>
2021-07-16 11:02:57 -0700

    Update

--- backend/build/config.js
+++ backend/build/config.js
@@ -19,8 +19,8 @@ var cookieExpiry = 365 * 52 * 7 * 24 * 60 * 60; // 2.21 days
 // http://expressjs.com/en/5x/api.html#res.cookie
 
 var cookieOptions = {
-  maxAge: 365 * 52 * 7,
-  expires: new Date(Date.now() + 365 * 52 * 7 * 1000),
+  maxAge: cookieExpiry,
+  expires: new Date(Date.now() + cookieExpiry),
   httpOnly: true,
   secure: production,
   path: '/',
