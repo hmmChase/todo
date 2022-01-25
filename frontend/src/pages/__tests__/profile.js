f7f72ab47b1974591b971cbe740a9a251b8e56a0
hmmChase <hmmChase@users.noreply.github.com>
2022-01-24 16:11:28 -0800

    Update

--- frontend/src/pages/__tests__/profile.js
+++ frontend/src/pages/__tests__/profile.js
@@ -1,35 +1,35 @@
-// import { renderApollo, cleanup, waitForElement } from '../../utils/test-utils';
-// import Profile, { CURRENT_USER } from '../profile';
-// import {} from '../../graphql/queries/user';
+import { renderApollo, cleanup, waitForElement } from '../../utils/test-utils';
+import AccountPage from '../account';
+import { CURRENT_USER } from '../../graphql/queries/user';
 
-// const mockIdea = {
-//   __typename: 'Idea',
-//   id: 1,
-//   content: 'aslkdfjlkasdjflksdjafl'
-// };
+const mockIdea = {
+  __typename: 'Idea',
+  id: 1,
+  content: 'aslkdfjlkasdjflksdjafl'
+};
 
-// const mockMe = {
-//   __typename: 'User',
-//   id: 1,
-//   email: 'a@a.a',
-//   ideas: [mockIdea]
-// };
+const mockMe = {
+  __typename: 'User',
+  id: 1,
+  email: 'a@a.a',
+  ideas: [mockIdea]
+};
 
-// describe('Profile Page', () => {
-//   // automatically unmount and cleanup DOM after the test is finished.
-//   afterEach(cleanup);
+describe('Account Page', () => {
+  // automatically unmount and cleanup DOM after the test is finished.
+  afterEach(cleanup);
 
-//   it('renders profile page', async () => {
-//     const mocks = [
-//       {
-//         request: { query: CURRENT_USER },
-//         result: { data: { me: mockMe } }
-//       }
-//     ];
+  it('renders account page', async () => {
+    const mocks = [
+      {
+        request: { query: CURRENT_USER },
+        result: { data: { me: mockMe } }
+      }
+    ];
 
-//     const { getByText } = renderApollo(<Profile />, { mocks });
+    const { getByText } = renderApollo(<AccountPage />, { mocks });
 
-//     // if the profile renders, it will have the list of missions booked
-//     await waitForElement(() => getByText(/test mission/i));
-//   });
-// });
+    // if the account renders, it will have the list of missions booked
+    await waitForElement(() => getByText(/test mission/i));
+  });
+});
