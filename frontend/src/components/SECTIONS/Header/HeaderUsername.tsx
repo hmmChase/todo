ea5d8d2ff6dbf31833e8766213bf55da34f8bb8b
hmmChase <hmmChase@users.noreply.github.com>
2022-04-16 18:13:42 -0700

    Update

--- frontend/src/components/SECTIONS/Header/HeaderUsername.tsx
+++ frontend/src/components/SECTIONS/Header/HeaderUsername.tsx
@@ -1,19 +1,18 @@
-// import React from 'react';
-// // import PropTypes from 'prop-types';
-// import { useQuery } from '@apollo/client';
+import React from 'react';
+import { useQuery } from '@apollo/client';
 
-// import { CURRENT_USER } from '../../../graphql/queries/user';
+import { CURRENT_USER } from '../../../graphql/queries/user';
 
-// const HeaderUsername = props => {
-//   const { loading, error, data } = useQuery(CURRENT_USER);
+const HeaderUsername = () => {
+  const { loading, error, data } = useQuery(CURRENT_USER);
 
-//   const currentUser = data?.currentUser;
+  const currentUser = data?.currentUser;
 
-//   return <p>You're signed in as {currentUser && currentUser.email}</p>;
-// };
+  return (
+    <p>
+      You{`&apos`}re signed in as {currentUser && currentUser.email}
+    </p>
+  );
+};
 
-// // HeaderUsername.propTypes = {
-// //   // myProp: PropTypes.string.isRequired
-// // };
-
-// export default React.memo(HeaderUsername);
+export default HeaderUsername;
