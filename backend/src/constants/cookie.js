8dacf89fbf903fbcfdc418de2c030ed43e70d65d
hmmChase <hmmChase@users.noreply.github.com>
2022-04-20 19:51:11 -0700

    Update

--- backend/src/constants/cookie.js
+++ backend/src/constants/cookie.js
@@ -13,9 +13,9 @@ console.log('`${frontendUrl}:${port}`:', `${frontendUrl}:${port}`);
 const accessCookieOptions = {
   httpOnly: true,
   maxAge: accessCookieExpiry,
-  sameParty: true, // allow cookies to be set by same origin
-  sameSite: development ? 'strict' : 'none', // deployed frontend/backend is cross-site
-  secure: !development,
+  // sameParty: true, // allow cookies to be set by same origin
+  sameSite: 'none', // deployed frontend/backend is cross-site
+  secure: true,
   // domain: '' //  hmmstart-backend.vercel.app
   // domain: production ? `hmmstart.vercel.app:${port}` : 'localhost'
   domain: development ? 'localhost' : 'vercel.app'
