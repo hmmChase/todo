378cb0da41d016c2888327d9758018b5aeb63501
hmmChase <hmmChase@users.noreply.github.com>
2022-04-20 19:21:16 -0700

    Update

--- backend/src/constants/cookie.js
+++ backend/src/constants/cookie.js
@@ -1,16 +1,24 @@
 // http://expressjs.com/en/5x/api.html#res.cookie
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Cookies
 
-import { accessCookieExpiry, development } from './config.js';
+import {
+  accessCookieExpiry,
+  development,
+  frontendUrl,
+  port
+} from './config.js';
+
+console.log('`${frontendUrl}:${port}`:', `${frontendUrl}:${port}`);
 
 const accessCookieOptions = {
   httpOnly: true,
   maxAge: accessCookieExpiry,
   sameParty: true, // allow cookies to be set by same origin
-  sameSite: !development ? 'none' : 'strict', // deployed frontend/backend is cross-site
+  sameSite: development ? 'strict' : 'none', // deployed frontend/backend is cross-site
   secure: !development,
-  domain: '' //  hmmstart-backend.vercel.app
+  // domain: '' //  hmmstart-backend.vercel.app
   // domain: production ? `hmmstart.vercel.app:${port}` : 'localhost'
+  domain: development ? 'localhost' : 'hmmstart-git-dev-hmmchase.vercel.app'
   // expires: new Date(Date.now() + accessCookieExpiry * 1000),
   // path: '/gql', // use '/' if cookie not showing in chrome
 };
