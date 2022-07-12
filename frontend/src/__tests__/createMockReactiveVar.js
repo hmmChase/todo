4c6ae6c36df0a21994bbaf6d9abc81274a6f4df9
hmmChase <hmmChase@users.noreply.github.com>
2022-07-11 21:39:51 -0700

    Update

--- frontend/src/__tests__/createMockReactiveVar.js
+++ frontend/src/__tests__/createMockReactiveVar.js
@@ -1,15 +1,15 @@
-// This is how to create a mock reactive variable.
-// It's a good idea to do this because then we can test our
-// interaction logic.
+// // This is how to create a mock reactive variable.
+// // It's a good idea to do this because then we can test our
+// // interaction logic.
 
-export const createMockReactiveVar = defaultValue => {
-  let currentValue = defaultValue;
+// export const createMockReactiveVar = defaultValue => {
+//   let currentValue = defaultValue;
 
-  const mockReactiveVar = newValue => {
-    if (newValue) currentValue = newValue;
+//   const mockReactiveVar = newValue => {
+//     if (newValue) currentValue = newValue;
 
-    return currentValue;
-  };
+//     return currentValue;
+//   };
 
-  return mockReactiveVar;
-};
+//   return mockReactiveVar;
+// };
