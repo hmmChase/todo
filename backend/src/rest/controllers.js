f21e104c509effc61e596b45695fc46fef4b2d68
hmmChase <hmmChase@users.noreply.github.com>
2023-02-28 21:49:17 -0800

    Update

--- backend/src/rest/controllers.js
+++ backend/src/rest/controllers.js
@@ -1,12 +1,4 @@
 /* index */
-import { backendUrl, frontendUrl } from '../constants/config.js';
 
 export const index = (req, res, next) =>
-  res.status(200).json({
-    route: 'index',
-    'backendUrl:': backendUrl,
-    'frontendUrl:': frontendUrl,
-    'process.env.NODE_ENV:': process.env.NODE_ENV,
-    'process.env.VERCEL_URL:': process.env.VERCEL_URL,
-    'process.env.VERCEL_ENV:': process.env.VERCEL_ENV
-  });
+  res.status(200).json({ route: 'index', ...process.env });
