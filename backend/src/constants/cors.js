f2dbde0edf036ea9b7493b1814144685453a2958
hmmChase <hmmChase@users.noreply.github.com>
2023-02-28 22:54:13 -0800

    Update

--- backend/src/constants/cors.js
+++ backend/src/constants/cors.js
@@ -1,15 +1,42 @@
-import { corsWhitelist } from './config.js';
+import {
+  // corsWhitelist,
+  development,
+  preview,
+  production
+} from './config.js';
+
+const frontendUrlDev = 'http://localhost:1337';
+const frontendUrlPrev = 'https://hmmstart-git-dev-hmmchase.vercel.app';
+const frontendUrlProd = 'https://hmmstart.vercel.app';
+
+export const frontendUrl = development
+  ? frontendUrlDev
+  : preview
+  ? frontendUrlPrev
+  : production && frontendUrlProd;
 
 // https://www.npmjs.com/package/cors#configuring-cors-w-dynamic-origin
 
-export const corsOptions = {
-  origin: function (origin, callback) {
-    if (corsWhitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+const corsDev = [frontendUrl, 'https://studio.apollographql.com'];
+const corsPrev = [frontendUrl];
+const corsProd = [frontendUrl];
 
-  credentials: true
-};
+const CORSwhitelist = development
+  ? corsDev
+  : preview
+  ? corsPrev
+  : production && corsProd;
+
+export const corsOptions = { origin: CORSwhitelist, credentials: true };
+
+// export const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (corsWhitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+
+//   credentials: true
+// };
