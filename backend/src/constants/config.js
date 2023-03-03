1f1688a41e6d10db7277a1bdb52ad11c1e9410dc
hmmChase <hmmChase@users.noreply.github.com>
2023-03-02 20:02:03 -0800

    Update

--- backend/src/constants/config.js
+++ backend/src/constants/config.js
@@ -33,7 +33,8 @@ const gitOwner = process.env.VERCEL_GIT_REPO_OWNER;
 const apolloStudio = 'https://studio.apollographql.com';
 
 const frontendUrlDev = 'http://localhost:1337';
-const frontendUrlPrev = `https://hmmstart-git-${gitBranch}-${gitOwner}.vercel.app`;
+// const frontendUrlPrev = `https://hmmstart-git-${gitBranch}-${gitOwner}.vercel.app`;
+const frontendUrlPrev = 'https://hmmstart-git-dev-hmmchase.vercel.app';
 const frontendUrlProd = 'https://hmmstart.vercel.app';
 
 const backendUrlDev = 'http://localhost:8008';
