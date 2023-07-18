b94a203de9c71519f2db641a5c79aba6838b1a0c
hmmChase <hmmChase@users.noreply.github.com>
2023-07-18 13:02:28 -0700

    Update

--- backend/src/constants/cors.js
+++ backend/src/constants/cors.js
@@ -3,3 +3,6 @@ import { apolloStudio, development, frontendUrl } from './config.js';
 const CORSwhitelist = development ? [apolloStudio, frontendUrl] : frontendUrl;
 
 export const corsOptions = { origin: CORSwhitelist, credentials: true };
+
+console.log('development:', development);
+console.log('corsOptions:', corsOptions);
